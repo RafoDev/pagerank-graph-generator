c5fba0d2f1beccb2179fddd62beb12eb7099f85e
RafoDev <rafael.david@ucsp.edu.pe>
2023-11-05 22:08:16 -0500

    fix(utilitary-scripts): little fixes on syntax.

--- clean.py
+++ clean.py
@@ -5,14 +5,4 @@ bucket_name = 'search-engine-bd'
 
 bucket = s3.Bucket(bucket_name)
 
-for obj in bucket.objects.filter(Prefix='/data'):
-    obj.delete()
-
-for obj in bucket.objects.filter(Prefix='/corpus/pdf'):
-    obj.delete()
-
-for obj in bucket.objects.filter(Prefix='/corpus/txt'):
-    obj.delete()
-
-for obj in bucket.objects.filter(Prefix='/corpus'):
-    obj.delete()
+bucket.objects.all().delete()
--- prepare-dirs.py
+++ prepare-dirs.py
@@ -3,11 +3,6 @@ import boto3
 s3_client = boto3.client('s3')
 bucket_name = 'search-engine-bd'
 
-object_key = 'mi_carpeta'
-
-contenido_del_archivo = 'Este es el contenido de mi archivo.'
-
 s3_client.put_object(Bucket=bucket_name, Key='data')
-s3_client.put_object(Bucket=bucket_name, Key='corpus')
 s3_client.put_object(Bucket=bucket_name, Key='corpus/pdf')
 s3_client.put_object(Bucket=bucket_name, Key='corpus/txt')
